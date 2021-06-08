4ecd3f9bec6fc515029241ffb53fbbe6ed767446
lpnhox <danmakuso1111@gmail.com>
2021-06-08 22:29:50 +0700

    Phi Truong them button

--- OOP_Desktop/MainChinh.cs
+++ OOP_Desktop/MainChinh.cs
@@ -16,5 +16,15 @@ namespace OOP_Desktop
         {
             InitializeComponent();
         }
+
+        private void MainChinh_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
