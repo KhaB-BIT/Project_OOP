f86af3bdb6ad2948c9511e00bb49ba9273d1a0cc
Truckhang181001 <Truckhang181001@gmail.com>
2021-06-10 17:55:34 +0700

    Update UI Design

--- OOP_Desktop/FormQuanLy.Designer.cs
+++ OOP_Desktop/FormQuanLy.Designer.cs
@@ -28,48 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLogoutQL = new System.Windows.Forms.Button();
-            this.btnMail = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormQuanLy));
+            this.btnDangXuat = new System.Windows.Forms.Button();
+            this.btnQlyNV = new System.Windows.Forms.Button();
             this.btnQLSach = new System.Windows.Forms.Button();
             this.btnDashboard = new System.Windows.Forms.Button();
             this.pnlQR = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.txtQR = new System.Windows.Forms.TextBox();
+            this.txtDashboard = new System.Windows.Forms.TextBox();
+            this.btnQlyKH = new System.Windows.Forms.Button();
+            this.btnQlyDoanhThu = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pnlQR.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnLogoutQL
+            // btnDangXuat
             // 
-            this.btnLogoutQL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
-            this.btnLogoutQL.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnLogoutQL.FlatAppearance.BorderSize = 0;
-            this.btnLogoutQL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogoutQL.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogoutQL.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnLogoutQL.Location = new System.Drawing.Point(0, 383);
-            this.btnLogoutQL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnLogoutQL.Name = "btnLogoutQL";
-            this.btnLogoutQL.Size = new System.Drawing.Size(279, 47);
-            this.btnLogoutQL.TabIndex = 10;
-            this.btnLogoutQL.Text = "Đăng xuất";
-            this.btnLogoutQL.UseVisualStyleBackColor = false;
+            this.btnDangXuat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
+            this.btnDangXuat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDangXuat.FlatAppearance.BorderSize = 0;
+            this.btnDangXuat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangXuat.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangXuat.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDangXuat.Location = new System.Drawing.Point(0, 556);
+            this.btnDangXuat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(279, 47);
+            this.btnDangXuat.TabIndex = 10;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.UseVisualStyleBackColor = false;
             // 
-            // btnMail
+            // btnQlyNV
             // 
-            this.btnMail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
-            this.btnMail.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnMail.FlatAppearance.BorderSize = 0;
-            this.btnMail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMail.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMail.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnMail.Location = new System.Drawing.Point(0, 312);
-            this.btnMail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnMail.Name = "btnMail";
-            this.btnMail.Size = new System.Drawing.Size(279, 47);
-            this.btnMail.TabIndex = 9;
-            this.btnMail.Text = "Quản lý nhân viên";
-            this.btnMail.UseVisualStyleBackColor = false;
+            this.btnQlyNV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
+            this.btnQlyNV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnQlyNV.FlatAppearance.BorderSize = 0;
+            this.btnQlyNV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQlyNV.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQlyNV.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnQlyNV.Location = new System.Drawing.Point(0, 402);
+            this.btnQlyNV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnQlyNV.Name = "btnQlyNV";
+            this.btnQlyNV.Size = new System.Drawing.Size(279, 47);
+            this.btnQlyNV.TabIndex = 9;
+            this.btnQlyNV.Text = "Quản lý nhân viên";
+            this.btnQlyNV.UseVisualStyleBackColor = false;
             // 
             // btnQLSach
             // 
@@ -79,7 +84,7 @@
             this.btnQLSach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnQLSach.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQLSach.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnQLSach.Location = new System.Drawing.Point(0, 242);
+            this.btnQLSach.Location = new System.Drawing.Point(0, 248);
             this.btnQLSach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnQLSach.Name = "btnQLSach";
             this.btnQLSach.Size = new System.Drawing.Size(279, 47);
@@ -109,7 +114,7 @@
             this.pnlQR.AllowDrop = true;
             this.pnlQR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(31)))), ((int)(((byte)(45)))));
             this.pnlQR.Controls.Add(this.pictureBox2);
-            this.pnlQR.Controls.Add(this.txtQR);
+            this.pnlQR.Controls.Add(this.txtDashboard);
             this.pnlQR.Location = new System.Drawing.Point(279, -3);
             this.pnlQR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlQR.Name = "pnlQR";
@@ -125,18 +130,63 @@
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
-            // txtQR
+            // txtDashboard
             // 
-            this.txtQR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(31)))), ((int)(((byte)(45)))));
-            this.txtQR.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtQR.Font = new System.Drawing.Font("r0c0i Linotte", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtQR.ForeColor = System.Drawing.Color.White;
-            this.txtQR.Location = new System.Drawing.Point(75, 41);
-            this.txtQR.Name = "txtQR";
-            this.txtQR.ReadOnly = true;
-            this.txtQR.Size = new System.Drawing.Size(267, 56);
-            this.txtQR.TabIndex = 2;
-            this.txtQR.Text = "Dashboard";
+            this.txtDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(31)))), ((int)(((byte)(45)))));
+            this.txtDashboard.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDashboard.Font = new System.Drawing.Font("r0c0i Linotte", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDashboard.ForeColor = System.Drawing.Color.White;
+            this.txtDashboard.Location = new System.Drawing.Point(75, 41);
+            this.txtDashboard.Name = "txtDashboard";
+            this.txtDashboard.ReadOnly = true;
+            this.txtDashboard.Size = new System.Drawing.Size(267, 56);
+            this.txtDashboard.TabIndex = 2;
+            this.txtDashboard.Text = "Dashboard";
+            // 
+            // btnQlyKH
+            // 
+            this.btnQlyKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
+            this.btnQlyKH.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnQlyKH.FlatAppearance.BorderSize = 0;
+            this.btnQlyKH.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQlyKH.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQlyKH.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnQlyKH.Location = new System.Drawing.Point(0, 325);
+            this.btnQlyKH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnQlyKH.Name = "btnQlyKH";
+            this.btnQlyKH.Size = new System.Drawing.Size(279, 47);
+            this.btnQlyKH.TabIndex = 12;
+            this.btnQlyKH.Text = "Quản lý khách hàng";
+            this.btnQlyKH.UseVisualStyleBackColor = false;
+            // 
+            // btnQlyDoanhThu
+            // 
+            this.btnQlyDoanhThu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
+            this.btnQlyDoanhThu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnQlyDoanhThu.FlatAppearance.BorderSize = 0;
+            this.btnQlyDoanhThu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQlyDoanhThu.Font = new System.Drawing.Font("r0c0i Linotte", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQlyDoanhThu.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnQlyDoanhThu.Location = new System.Drawing.Point(0, 479);
+            this.btnQlyDoanhThu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnQlyDoanhThu.Name = "btnQlyDoanhThu";
+            this.btnQlyDoanhThu.Size = new System.Drawing.Size(279, 47);
+            this.btnQlyDoanhThu.TabIndex = 13;
+            this.btnQlyDoanhThu.Text = "Quản lý doanh thu";
+            this.btnQlyDoanhThu.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::OOP_Desktop.Properties.Resources.FinalPng;
+            this.pictureBox1.InitialImage = global::OOP_Desktop.Properties.Resources.FinalPng;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
             // 
             // FormQuanLy
             // 
@@ -144,31 +194,39 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(8)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(1311, 655);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnQlyDoanhThu);
+            this.Controls.Add(this.btnQlyKH);
             this.Controls.Add(this.pnlQR);
-            this.Controls.Add(this.btnLogoutQL);
-            this.Controls.Add(this.btnMail);
+            this.Controls.Add(this.btnDangXuat);
+            this.Controls.Add(this.btnQlyNV);
             this.Controls.Add(this.btnQLSach);
             this.Controls.Add(this.btnDashboard);
             this.Font = new System.Drawing.Font("r0c0i Linotte", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormQuanLy";
             this.Text = "Phần mềm quản lý bán sách";
             this.Load += new System.EventHandler(this.MainChinh_Load);
             this.pnlQR.ResumeLayout(false);
             this.pnlQR.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnLogoutQL;
-        private System.Windows.Forms.Button btnMail;
+        private System.Windows.Forms.Button btnDangXuat;
+        private System.Windows.Forms.Button btnQlyNV;
         private System.Windows.Forms.Button btnQLSach;
         private System.Windows.Forms.Button btnDashboard;
         private System.Windows.Forms.Panel pnlQR;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox txtQR;
+        private System.Windows.Forms.TextBox txtDashboard;
+        private System.Windows.Forms.Button btnQlyKH;
+        private System.Windows.Forms.Button btnQlyDoanhThu;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
